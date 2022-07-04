1a6052b14e6ebaec1d0cc3838ec9b5be2360447d
Nick Pisano <pisano.nicholas@yahoo.com>
2022-07-04 05:27:58 -0700

    Added More Code

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,7 +1,9 @@
+var totalSeconds = 0;
+var timerVariable = setInterval(raidTimer, 1000);
 var checkLsArr = [true]
 let checker = arr => arr.every(Boolean);
 var btnIndex = 9;
-startRaidUp(false);
+startRaidBtn(false);
 
 
 for (let i = 1; i < btnIndex; i++) {
@@ -13,30 +15,49 @@ for (let i = 1; i < btnIndex; i++) {
             $(`#${idClicked}`).addClass("btn btn-outline-success mb-2");
             checkLsArr[i] = true;
             let checker = arr => arr.every(Boolean);
-            startRaidUp(checker(checkLsArr));
+            startRaidBtn(checker(checkLsArr));
         } else {
             $(`#${idClicked}`).removeClass("btn btn-outline-success mb-2");
             $(`#${idClicked}`).addClass("btn btn-outline-danger mb-2");
             checkLsArr[i] = false;
             let checker = arr => arr.every(Boolean);
-            startRaidUp(checker(checkLsArr));
+            startRaidBtn(checker(checkLsArr));
         }
     });
 };
 
-function startRaidUp(res) {
+function startRaidBtn(res) {
     if (res === true) {
         $(document).ready(function () {
             $('#startMatchBtn').delay(1).fadeIn('fast');
         });
         $("#startMatchBtn").click(function () {
-            alert("Handler for .click() called.");
+            displayStats();
+            $(document).ready(function () {
+                $('#raidTime').delay(1).fadeIn('fast');
+            });
         });
     } else {
         $(document).ready(function () {
             $('#startMatchBtn').delay(1).fadeOut('fast');
         });
+        $(document).ready(function () {
+            $('#raidTime').delay(1).fadeOut('fast');
+        });
     }
+}
+
+function displayStats() {
+    alert("Handler for .click() called.");
+    raidTimer();
+}
+
+function raidTimer() {
+    ++totalSeconds;
+    var hour = Math.floor(totalSeconds / 3600);
+    var minute = Math.floor((totalSeconds - hour * 3600) / 60);
+    var seconds = totalSeconds - (hour * 3600 + minute * 60);
+    document.getElementById("count_up_timer").innerHTML = hour + ":" + minute + ":" + seconds;
 }
 
 setInterval(currentTime, 1000);
