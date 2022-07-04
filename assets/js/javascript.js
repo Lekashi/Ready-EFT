931acde45dfbc9543b5384db27bcffc62e5a9ad3
Nick Pisano <pisano.nicholas@yahoo.com>
2022-07-03 19:07:08 -0700

    Added checking if your ready for raid prototype

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,9 +1,4 @@
-setInterval(currentTime, 1000);
-
-function currentTime() {
-    var displayedTime = moment().format("LTS");
-    $("#currentTime").text(displayedTime);
-}
+var checkLsArr = [true]
 
 for (let i = 1; i < 9; i++) {   
     $(`#checklistitem${i}`).click(function(event) {
@@ -11,9 +6,19 @@ for (let i = 1; i < 9; i++) {
         if ($(`#${idClicked}`).hasClass("btn btn-outline-danger mb-2") === true) {
             $(`#${idClicked}`).removeClass("btn btn-outline-danger mb-2");
             $(`#${idClicked}`).addClass("btn btn-outline-success mb-2");
+            checkLsArr[i] = true;
         } else {
             $(`#${idClicked}`).removeClass("btn btn-outline-success mb-2");
             $(`#${idClicked}`).addClass("btn btn-outline-danger mb-2");
+            checkLsArr[i] = false;
         }
-      });
+        console.log(checkLsArr);
+    });
+};
+
+setInterval(currentTime, 1000);
+
+function currentTime() {
+    var displayedTime = moment().format("LTS");
+    $("#currentTime").text(displayedTime);
 }
