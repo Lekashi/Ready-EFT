ac88d657e88a759dac70fa9014f358f07ab7e209
Nick Pisano <pisano.nicholas@yahoo.com>
2022-07-03 18:00:21 -0700

    Added event listners to checklist buttons

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -4,3 +4,16 @@ function currentTime() {
     var displayedTime = moment().format("LTS");
     $("#currentTime").text(displayedTime);
 }
+
+for (let i = 1; i < 9; i++) {   
+    $(`#checklistitem${i}`).click(function(event) {
+        var idClicked = event.target.id;
+        if ($(`#${idClicked}`).hasClass("btn btn-outline-danger mb-2") === true) {
+            $(`#${idClicked}`).removeClass("btn btn-outline-danger mb-2");
+            $(`#${idClicked}`).addClass("btn btn-outline-success mb-2");
+        } else {
+            $(`#${idClicked}`).removeClass("btn btn-outline-success mb-2");
+            $(`#${idClicked}`).addClass("btn btn-outline-danger mb-2");
+        }
+      });
+}
